654871fe6d397287b92396d0e17ddf1820541347
HarimKim <lklkcmk@naver.com>
2017-06-22 08:03:20 -0700

    1

--- collection/map/newMap.java
+++ collection/map/newMap.java
@@ -1,16 +1,54 @@
-public class newMap{
-	private <T> key;
-	private <T> value;
-	private ArrayList<T> arr_k;
-	private ArrayList<T> arr_v;
+import java.util.ArrayList;
+
+public class newMap<K,V>{
+	//private K key;
+	//private V value;
+	private ArrayList<K> keyArray=new ArrayList<>(); //keyArray : array of key
+	private ArrayList<V> valArray=new ArrayList<>(); //valArray : array of value
 	
-	public void put(T key, T value){}
-	public <T> get(T key){return value;} 
-	public boolean constainsKey(T key){return true;}
-	public boolean constainsValue(T value){return true;}
-	public ArrayList<T> getKey(){return arr_k;}
-	public ArrayList<T> getValue(){return arr_v;}
-	public void clear(){}
+	public void put(K key, V value){
+		int keyIndex;
+		if(keyArray.contains(key)){
+			keyIndex=keyArray.indexOf(key);
+			valArray.set(keyIndex,value);
+		}
+		else{
+			keyArray.add(key);
+			valArray.add(value);	
+		}
+	}
+	public V get(K key){
+		int keyIndex;
+		V value;
+
+		if(keyArray.contains(key)){
+			keyIndex=keyArray.indexOf(key);
+			value=valArray.get(keyIndex);
+			return value;
+		}
+		else
+			return null;
+	} 
+	public boolean containsKey(K key){
+		boolean temp;
+		temp=keyArray.contains(key);
+		return temp;
+	}
+	public boolean containsValue(V value){
+		boolean temp;
+		temp=valArray.contains(value);
+		return temp;
+	}
+	public ArrayList<K> getKey(){
+		return keyArray;
+	}
+	public ArrayList<V> getValue(){
+		return valArray;
+	}
+	public void clear(){
+		keyArray.clear();
+		valArray.clear();
+	}
 
 }
 
