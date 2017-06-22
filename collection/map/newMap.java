c654fb6d8686d1e0a7f63fa750f0770b26868332
HarimKim <lklkcmk@naver.com>
2017-06-22 12:11:02 -0700

    2

--- collection/map/newMap.java
+++ collection/map/newMap.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
 
+/** 
+ *2017_Summer GBC Assignment : Map
+ *@author : semi,harim
+ */ 
+
 public class newMap<K,V>{
-	//private K key;
-	//private V value;
-	private ArrayList<K> keyArray=new ArrayList<>(); //keyArray : array of key
-	private ArrayList<V> valArray=new ArrayList<>(); //valArray : array of value
 	
+	private ArrayList<K> keyArray=new ArrayList<>(); /**keyArray : array of key*/
+	private ArrayList<V> valArray=new ArrayList<>(); /**valArray : array of value*/
+	
+	/**
+	 *put the value and key in pair in the map
+	 *@param key - the key data to put in the map
+	 *@param value - the value data to put in the map
+	 */
 	public void put(K key, V value){
 		int keyIndex;
 		if(keyArray.contains(key)){
@@ -17,6 +26,12 @@ public class newMap<K,V>{
 			valArray.add(value);	
 		}
 	}
+	
+	/**
+	 *get the value that make a pair with the key from the map
+	 *@param key - the key data to find a value
+	 *@return the value matched up with the key
+	 */
 	public V get(K key){
 		int keyIndex;
 		V value;
@@ -29,22 +44,48 @@ public class newMap<K,V>{
 		else
 			return null;
 	} 
+
+	/**
+	 *check that the key data is or not 
+	 *@param key - the key data to check
+	 *@return true or false
+	 */
 	public boolean containsKey(K key){
 		boolean temp;
 		temp=keyArray.contains(key);
 		return temp;
 	}
+
+	/**
+	 *check that the value is or not
+	 *@param value - the value to check
+	 *@return true or false
+	 */
 	public boolean containsValue(V value){
 		boolean temp;
 		temp=valArray.contains(value);
 		return temp;
 	}
+
+	/**
+	 *return all key datas
+	 *@return ArrayList that have the key datas
+	 */
 	public ArrayList<K> getKey(){
 		return keyArray;
 	}
+
+	/**
+	 *return all values
+	 *@return ArrayList that have the values
+	 */
 	public ArrayList<V> getValue(){
 		return valArray;
 	}
+
+	/**
+	 *delete all the data in the map
+	 */
 	public void clear(){
 		keyArray.clear();
 		valArray.clear();
